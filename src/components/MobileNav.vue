f33cb0206a4e125224491b14887a35472aec2c8c
tina234 <martina.dejanovic94@gmail.com>
2022-04-16 21:24:26 +0200

    hamb dropdown

--- src/components/MobileNav.vue
+++ src/components/MobileNav.vue
@@ -1,6 +1,28 @@
 <template>
-    <div class="hamb-icon">
-        <i class="fa fa-solid fa-bars"></i>
+    <div>
+        <div class="hamb-icon">
+            <i class="fa fa-solid fa-bars"></i>
+        </div>
+        <transition>
+            <div class="sidebar">
+                <router-link class="nav-item" v-for="category in categories" :key="category.title" :to="category.link">{{ category.title }}</router-link>
+            </div>
+        </transition>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            categories : "",
+        }
+    },
+
+     created() {
+        this.categories = this.$store.state.categories_modul.categories;
+        console.log(this.categories);
+    }
+}
+</script>
 

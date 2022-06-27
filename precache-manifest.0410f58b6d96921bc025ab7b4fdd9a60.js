56f027c159163a22d907f8dbdb4dbe911c47af9b
tina234 <martina.dejanovic94@gmail.com>
2022-06-27 21:19:24 +0200

    New Deployment

---
+++ precache-manifest.0410f58b6d96921bc025ab7b4fdd9a60.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d9ce1ea31439c4af3ab",
+    "url": "/coffee_shop/css/app.93012bf8.css"
+  },
+  {
+    "revision": "bee505966308b2e35f24cba72533ce8b",
+    "url": "/coffee_shop/img/cbag1.bee50596.png"
+  },
+  {
+    "revision": "36bcab0e021076771241609bf1cf857e",
+    "url": "/coffee_shop/img/cbag2.36bcab0e.png"
+  },
+  {
+    "revision": "c94ad090e2d92311d34e6d9c37be6c9b",
+    "url": "/coffee_shop/img/cbag3.c94ad090.png"
+  },
+  {
+    "revision": "183842941564a11cdc3bac0d83ffab8e",
+    "url": "/coffee_shop/img/cbag4.18384294.png"
+  },
+  {
+    "revision": "4c783fa4e7b1274c2a45fed8a0f055cd",
+    "url": "/coffee_shop/img/cbag5.4c783fa4.png"
+  },
+  {
+    "revision": "98ba84520add5600b8397ff5a85d5758",
+    "url": "/coffee_shop/img/coffee-bean.98ba8452.png"
+  },
+  {
+    "revision": "92482048bfaeb3bacd28328428d0b25e",
+    "url": "/coffee_shop/img/coffee-bean33.92482048.png"
+  },
+  {
+    "revision": "443f7842ee265b8dc5f9398bc7afa44a",
+    "url": "/coffee_shop/img/coffeebeans.443f7842.png"
+  },
+  {
+    "revision": "5c08f4bc4d509041fbaf0e5e1c64ba3f",
+    "url": "/coffee_shop/img/coffeebeans.5c08f4bc.jpg"
+  },
+  {
+    "revision": "9ea58b47b164ee05d29c8aa2248193ff",
+    "url": "/coffee_shop/img/coffeebeans33.9ea58b47.png"
+  },
+  {
+    "revision": "02ee68e1ea805948d676a24732931b63",
+    "url": "/coffee_shop/img/coffeebeans34.02ee68e1.png"
+  },
+  {
+    "revision": "f3de6ab44ae854e677d76ac7bb806485",
+    "url": "/coffee_shop/img/coffeecup.f3de6ab4.jpg"
+  },
+  {
+    "revision": "169d6f684545297e7b39c5124a4e190a",
+    "url": "/coffee_shop/img/facebook (1).169d6f68.png"
+  },
+  {
+    "revision": "f9e6b5a851536fa7b414e82edef6493d",
+    "url": "/coffee_shop/img/free-delivery.f9e6b5a8.png"
+  },
+  {
+    "revision": "7beefa38edf272037c333de6818d575f",
+    "url": "/coffee_shop/img/gift.7beefa38.png"
+  },
+  {
+    "revision": "e00908e9d7c4919287edda4d863e23b4",
+    "url": "/coffee_shop/img/headerimg.e00908e9.jpg"
+  },
+  {
+    "revision": "0ff3809ea27d235d92abd8064395afc2",
+    "url": "/coffee_shop/img/instagram.0ff3809e.png"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/coffee_shop/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "29ea88a02ca0845dd213fc86f6d85bb8",
+    "url": "/coffee_shop/img/twitter.29ea88a0.png"
+  },
+  {
+    "revision": "5228dc9cb321736823244f4bfb34da4e",
+    "url": "/coffee_shop/index.html"
+  },
+  {
+    "revision": "8d9ce1ea31439c4af3ab",
+    "url": "/coffee_shop/js/app.4140eacb.js"
+  },
+  {
+    "revision": "5c0286baa4c5304061f3",
+    "url": "/coffee_shop/js/chunk-vendors.5df83e3a.js"
+  },
+  {
+    "revision": "652278626e7cd4955b9897db404c0703",
+    "url": "/coffee_shop/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/coffee_shop/robots.txt"
+  }
+]);
